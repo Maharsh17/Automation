8ffb13e46bc1e49be93aaef2b25c6b8aa1d26ed3
Maharsh17 <56247692+Maharsh17@users.noreply.github.com>
2021-05-01 13:12:15 +0530

    Add files via upload

---
+++ Linux/deb/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+echo "Here Are The Software We Are Going To Install"
+
+echo "Chrome"
+echo "Visual Code"
+echo "Atom"
+echo "Teams"
+echo "Zoom"
+echo "JDK"
+echo "Python-3"
+
+cd
+cd Downloads
+
+git clone https://gitlab.com/janimaharsh/deb.git
+unzip
