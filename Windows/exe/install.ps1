f9845f3e445ed122d9f3c93af006f99d2c9f50dd
Maharsh17 <56247692+Maharsh17@users.noreply.github.com>
2021-05-01 09:22:51 +0530

    Add files via upload

---
+++ Windows/exe/install.ps1
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+echo "Here Are The Software We Are Going To Install"
+
+echo "Chrome"
+echo "Visual Code"
+echo "Atom"
+echo "Teams"
+echo "Zoom"
+echo "JDK"
+echo "Python-3"
+
+cd
+cd Downloads
+
+curl https://gitlab.com/janimaharsh/windows/-/archive/master/windows-master.tar
+tar -xf windows-master.tar 
+
+
